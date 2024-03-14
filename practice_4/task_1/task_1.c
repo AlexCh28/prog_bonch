995861b4dcaaf2b7e9da073b069e7a8cf7b5f101
Alexander Chaschin <sania.chaschin2011@yandex.ru>
2024-03-14 20:38:24 +0300

    practice 4 task 1

--- practice_4/task_1/task_1.c
+++ practice_4/task_1/task_1.c
@@ -6,25 +6,72 @@ const char OUTPUT_FILE_NAME[] = "output";
 
 int main()
 {
-    char inputString[INPUT_LENGTH];
+    int mode;
+    char userFileName[INPUT_LENGTH];
+
     FILE *inputPtr, *outputPtr;
 
     inputPtr = fopen(INPUT_FILE_NAME, "r");
-    outputPtr = fopen(OUTPUT_FILE_NAME, "w");
-
     if (inputPtr == NULL)
     {
         printf("Файл %s не был найден\n", INPUT_FILE_NAME);
         return 0;
     }
 
-    fgets(inputString, INPUT_LENGTH, inputPtr);
-    printf("%s\n", inputString);
+    printf("Куда стоит скопировать содержимое файла [1 - копировать в файл, 2 - вывести в консоль]: ");
+    scanf("%d", &mode);
 
-    fprintf(outputPtr, "test write to file from program");
+    if (mode == 1)
+    {
+        printf("Введите название для нового файла: ");
+        scanf("%s", userFileName);
 
-    fclose(inputPtr);
-    fclose(outputPtr);
+        outputPtr = fopen(userFileName, "w");
+        if (outputPtr == NULL)
+        {
+            printf("Файл %s не был найден\n", userFileName);
+            fclose(inputPtr);
+            return 0;
+        }
 
-    return 0;
+        while(!feof(inputPtr))
+        {
+            char c = fgetc(inputPtr);
+            if (c == -1)
+            {
+                break;
+            }
+            fprintf(outputPtr, "%c", c);
+        }
+
+        printf("Содержимое файла %s скопировано в файл %s\n", INPUT_FILE_NAME, userFileName);
+
+        fclose(inputPtr);
+        fclose(outputPtr);
+
+        return 0;
+    }
+    else if (mode == 2)
+    {
+        while(!feof(inputPtr))
+        {
+            char c = fgetc(inputPtr);
+            if (c == -1)
+            {
+                break;
+            }
+            printf("%c", c);
+        }
+        printf("\n");
+        printf("Содержимое файла %s скопировано в консоль\n", INPUT_FILE_NAME);
+
+        fclose(inputPtr);
+
+        return 0;
+    }
+    else
+    {
+        printf("Вы выбрали несуществующую опцию.\n");
+        return 0;
+    }
 }
